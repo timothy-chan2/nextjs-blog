5e3fce352e798c23f87fd86ba3b9e7cbf19bcf07
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-08-27 17:15:17 -0400

    Add Layout component used for local styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,11 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
+import Layout from '../../components/layout';
+
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -11,6 +13,6 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
