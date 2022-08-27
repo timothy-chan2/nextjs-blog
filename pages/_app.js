d1c1bc82db96d4cc28ba78635f654434555716c1
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-08-27 17:29:49 -0400

    Add global styles

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// Global CSS must only be imported into this file
+import '../styles/global.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
