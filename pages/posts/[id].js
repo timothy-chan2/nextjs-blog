d3acaaaff28a1e300baf7dd0a845db4a87cbf4d5
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-08-30 13:42:22 -0400

    Add title and update to use a more human readable date

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,6 @@
+import Head from 'next/head';
 import Layout from '../../components/layout';
+import Date from '../../components/date';
 
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
@@ -22,11 +24,14 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date} />
       <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
