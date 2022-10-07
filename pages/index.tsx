183bfbf2db9651190cef1b1dbdf2bdf83e6259da
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-10-06 23:45:47 -0400

    Convert JavaScript file into TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { GetStaticProps } from 'next';
+
 import Layout, { siteTitle } from '../components/layout';
 import Date from '../components/date';
 
@@ -8,7 +10,7 @@ import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 
 // getStaticProps only runs on the server-side
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
@@ -17,7 +19,15 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string,
+    title: string,
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
